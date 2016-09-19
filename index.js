61b348d0d0923e9e6a417cc80c33ab447355fecc
leakvoid <leakvoid@gmail.com>
2016-09-19 16:34:25 +0300

    header parser microservice solution has been implemented

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 
 var express = require('express');
 var app = express();
+var useragent = require('useragent');
 
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    console.log('test test test');
-    res.end('test test test');
+    var ip = req.connection.remoteAddress ||
+	req.headers["x-forwarded-for"] ||
+	req.socket.remoteAddress ||
+	req.connection.socket.remoteAddress;
+    console.log("IP_address: " + ip);
+
+    var language = req.headers["accept-language"];
+    console.log("Language: " + language);
+
+    var agent = useragent.parse(req.headers["user-agent"]);
+    console.log("OS: " + agent.os.toString());
+
+    res.json({"IP_address: ": ip,
+	     "Language: ": language,
+	     "OS: ": agent.os.toString()});
 });
 
 app.listen(port, function() {
